9a9021dafef0c5633c9724d33d44239615a642fe
张迪 <codeway3@126.com>
2017-12-25 09:44:34 +0800

    satisfy PEP8

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+import models
 
 # grabs the folder where the script runs
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -25,8 +26,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 app = Flask(__name__)
 app.config.from_object(__name__)
 db = SQLAlchemy(app)
-
-import models
 
 
 @app.route('/')
